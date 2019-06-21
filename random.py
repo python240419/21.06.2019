e35c8ad1509d94bea47e233ff17b8bf7c368b2da
python240419 <51232248+python240419@users.noreply.github.com>
2019-06-21 11:04:08 +0300

    Update random.py

--- random.py
+++ random.py
@@ -1,13 +1,31 @@
 import random
 
-# create a ranmod number
-# input numbers from the user until the user guessed
-#   the number
-# if the user guessed higher then print 'too high'
-# if the user guessed lower then print 'too low'
-# ...print how many attempts?
-
 for x in range(10):
     print(random.randint(1, 10))
 
-print(random.uniform(1, 2)) # random float number
+#print(random.uniform(1, 2)) # random float number
+
+# create a ranmod number
+# input numbers from the user until the user guessed
+#   the number
+# if the user guessed higher then -> print 'too high'
+# if the user guessed lower then -> print 'too low'
+# ...print how many attempts
+
+luckyNumber = random.randint(1, 100)
+attempts = 1
+myGuess = int(input("Guess the number:"))
+while myGuess != luckyNumber :
+    if myGuess > luckyNumber:
+        print('Too high')
+    else:
+        print('Too low')
+    attempts = attempts + 1
+    myGuess = int(input("Guess the number:"))
+
+print(f'Well done! it tooks you {attempts} attempts')
+
+
+
+
+
