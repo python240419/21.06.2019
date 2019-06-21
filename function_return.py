44b94227eb731cae242e2e033c43d970e6deefc1
python240419 <51232248+python240419@users.noreply.github.com>
2019-06-21 12:18:04 +0300

    Create function_return.py

---
+++ function_return.py
@@ -0,0 +1,23 @@
+
+def getAreaCircle(rad):
+    #area = rad ** 2 * 3.14
+    #return area
+    return rad ** 2 * 3.14
+
+def foo():
+    print('hello from foo')
+
+#length = len('abc')
+radius = 3.6
+theArea1 = getAreaCircle(2.4)
+theArea2 = getAreaCircle(7.9)
+theArea3 = getAreaCircle(radius)
+myCircles = [getAreaCircle(2.4), getAreaCircle(2.2)]
+print(theArea3)
+print(getAreaCircle(radius))
+a = foo()
+print(a)
+print(type(a))
+
+x = print("name:")
+print(x)
