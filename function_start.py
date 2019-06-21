62d10c1ee165c559b60a07f0fcb1233ce4c18c75
python240419 <51232248+python240419@users.noreply.github.com>
2019-06-21 11:14:19 +0300

    Create function_start.py

---
+++ function_start.py
@@ -0,0 +1,16 @@
+
+def function1():
+    print('hello world!')
+
+def printNumbers1to100():
+    for n in range(1, 100):
+        print(n)
+        if n % 2 == 0:
+            print("even")
+
+function1()
+printNumbers1to100()
+function1()
+function1()
+function1()
+printNumbers1to100()
